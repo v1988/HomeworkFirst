e399cc30de0a2404f005fc4500e06f0150e5ffd4
v1988 <evgvers2@gmail.com>
2020-06-04 00:36:10 +0300

    fix

--- src/main/java/Lesson1/Homework.java
+++ src/main/java/Lesson1/Homework.java
@@ -10,50 +10,52 @@ public class Homework { // #1
         float f = 14.88F;
         char c = '0';
         boolean bo = false;
-        String myString = "Я пытался, но не вышло";
+
+        String myString = "Я пытался, вышло сомнительно";
         System.out.println(myString);
-        String myString2 = new String("Я пытался, но не вышло");
+        String myString2 = new String("Я пытался, вышло сомнительно");
         System.out.println(myString2);
+
         int a = 1, j = 7, r = 9;
         double d = 1.45, p = 2.66, q = 8.09;
+
         //арифметические действия #3
         double result = b + s - f;
-        System.out.print(result); // дробный результат 1
+        System.out.print(result); // дробный результат 1 (30085.119140625)
         System.out.print("   ");
 
         double n = b * 3 + f / 2 - s;
-        System.out.print(n); // дробный результат 2
+        System.out.print(n); // дробный результат 2 (-29692.560546875)
         System.out.print("   ");
 
-        int k = 10;
-        double resultA = k / f;
-        System.out.println(resultA); // дробный результат 3
+        double resultA = 10 / f;
+        System.out.println(resultA); // дробный результат 3 (0.6720430254936218)
 
-        double resultB = k * 1.46 / 2;
-        System.out.print(resultB); // дробный результат 4
+        double resultB = 10 * 1.46 / 2;
+        System.out.print(resultB); // дробный результат 4 (7.3)
         System.out.print("   ");
 
-        double resultC = f * k / 3.45;
-        System.out.println(resultC); // дробный результат 5
+        double resultC = f * 10 / 3.45;
+        System.out.println(resultC); // дробный результат 5 (43.13043566717617)
         System.out.println("   ");
 
-        int resultD = 50000 / b + k;
-        System.out.print(resultD); // целый результат 1
+        int resultD = 50000 / b + 10;
+        System.out.print(resultD); // целый результат 1 (510)
         System.out.print("   ");
 
-        int resultE = s * k / b;
-        System.out.print(resultE); // целый результат 2
+        int resultE = s * 10 / b;
+        System.out.print(resultE); // целый результат 2 (3000)
         System.out.print("   ");
 
-        int resultF = b - 1000 / k;
-        System.out.println(resultF); // целый результат 3
+        int resultF = b - 1000 / 10;
+        System.out.println(resultF); // целый результат 3 (0)
 
-        long resultG = l - k + b;
-        System.out.print(resultG); // целый результат 4
+        long resultG = l - 10 + b;
+        System.out.print(resultG); // целый результат 4 (10090)
         System.out.print("   ");
 
-        int resultH = 150 / b * k;
-        System.out.println(resultH); // целый результат 5
+        int resultH = 150 / b * 10;
+        System.out.println(resultH); // целый результат 5 (10)
         System.out.println("   ");
 
         // с присвоением #4
@@ -62,29 +64,28 @@ public class Homework { // #1
         int third = -65;
         int fourth = 100;
         int fifth = 10;
-        System.out.print(first *= fifth); // присвоение 1
+        System.out.print(first *= fifth); // присвоение 1 (500)
         System.out.print("   ");
-        System.out.print(fourth /= 50); // присвоение 6
+        System.out.print(fourth /= 50); // присвоение 2 (2)
         System.out.print("   ");
-        System.out.print(fourth -= fourth); // присвоение 2
+        System.out.print(fourth -= fourth); // присвоение 3 (0)
         System.out.print("   ");
-        System.out.print(second += fifth); // присвоение 3
+        System.out.print(second += fifth); // присвоение 4 (710)
         System.out.print("   ");
-        System.out.print(fifth -= third); // присвоение 4
+        System.out.print(fifth -= third); // присвоение 5 (75)
         System.out.print("   ");
-        System.out.println(third %= first); // присвоение 5
+        System.out.println(third %= first); // присвоение 6 (-65)
         System.out.print("   ");
 
         // логика #5
-        System.out.print(result > n);
+        System.out.print(result > n); // true
         System.out.print("   ");
-        System.out.print(n != result);
+        System.out.print(n != result); // true
         System.out.print("   ");
-        System.out.print(n == s);
+        System.out.print(n == s); // false
         System.out.print("   ");
-        System.out.print(result >= l);
+        System.out.print(result >= l); // false
         System.out.print("   ");
-        System.out.print(result >= l || b > n);
-// test 123
+        System.out.print(result >= l || b > n); // true
     }
 }
