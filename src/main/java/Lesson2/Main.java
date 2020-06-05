4d99af6a1605d821e143fa8d3a29e2cfb7604274
v1988 <evgvers2@gmail.com>
2020-06-05 17:08:16 +0300

    version 1.0

---
+++ src/main/java/Lesson2/Main.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "Допустим";
+        String str2 = str1;
+        System.out.println(str1 + " я это сделал");
+        System.out.println(str2);
+        int var = 10;
+        System.out.println(var);
+        var++;
+        System.out.println(var);
+    }
+}
